210eba2dbad72372755670c3fe8a6cd9b9d5a3d3
Opilane TTHK <opilane@tthk.ee>
2022-02-23 12:48:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kordamine
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Variant 5 Ü1
+            var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u' };
+            int vows = 0;
+
+            Console.WriteLine("Enter a Sentence");
+            string sentence = Console.ReadLine().ToLower();
+            int len = sentence.Length;
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (vowels.Contains(sentence[i]))
+                {
+                    vows++;
+                }
+            }
+            float total = ((float)((decimal)vows / len * 100));
+            
+            Console.WriteLine("Your total percentage of vowels is: {0}", total);
+
+        }
+    }
+}
